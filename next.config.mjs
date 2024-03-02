6756670cbb2b7427e37387c787819bacc82de4e6
GeroGambuli <gerogambuli2002@gmail.com>
2024-03-01 21:15:51 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com' // if your website has no www, drop it
+      }
+    ]
   }
 }
 
